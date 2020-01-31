73d5da5c05b56b954a025ca5953e4aeb1d1d75aa
LaoYang1994 <lhy_ustb@pku.edu.cn>
2020-01-31 13:50:59 +0800

    update dataset relevant

--- sognet/data/dataset.py
+++ sognet/data/dataset.py
@@ -1,5 +1,11 @@
-from detectron2.data import DatasetCatalog, MetadataCatalog
+import os
 
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.data.datasets import register_coco_panoptic_separated, _get_builtin_metadata
+
+
+def get_sog_metadata():
+    pass
 
 _PREDEFINED_SPLITS_COCO_PANOPTIC_SOG = {
     "coco_2017_train_panoptic_sog": (
@@ -19,3 +25,21 @@ _PREDEFINED_SPLITS_COCO_PANOPTIC_SOG = {
         "coco/panoptic_all_cat_val2017",
     )
 }
+
+
+for (
+    prefix,
+    (panoptic_root, panoptic_json, semantic_root),
+) in _PREDEFINED_SPLITS_COCO_PANOPTIC_SOG.items():
+    prefix_instances = prefix[: -len("_panoptic_sog")]
+    instances_meta = MetadataCatalog.get(prefix_instances)
+    image_root, instances_json = instances_meta.image_root, instances_meta.json_file
+    register_coco_panoptic_separated(
+        prefix,
+        _get_builtin_metadata("coco_panoptic_separated"),
+        image_root,
+        os.path.join("datasets", panoptic_root),
+        os.path.join("datasets", panoptic_json),
+        os.path.join("datasets", semantic_root),
+        instances_json,
+    )
